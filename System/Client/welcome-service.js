a229585a40b15db4689403738d931ba86af00277
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-01-10 04:17:42 -0600

    Fix welcome-service.js

--- System/Client/welcome-service.js
+++ System/Client/welcome-service.js
@@ -18,9 +18,7 @@ function WS(client) {
 			color: 13193877,
 			description: `Welcome **${
 				member.user.username
-			}**! We are glad to see you here, Now you are officially part **of Alapha Universe**\n\nPlease read the #rules and also get some nice roles from #self-roles. **And don't forget to say hi!**\n\n**Account created on** <t:${Math.round(
-				member.user.createdTimestamp / 1000
-			)}:f>`,
+			}**! We are glad to see you here, Now you are officially part **of Alapha Universe**\n\nPlease read the #rules and also get some nice roles from #self-roles. **And don't forget to say hi!**`,
 			timestamp: new Date(),
 			author: {
 				name: "",
