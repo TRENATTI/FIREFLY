03e9f978d0e357ef06be97dc7a2eca160addc6d1
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-01-23 03:00:19 -0600

    Update blacklist.js -> DEMO BLACKLSIT WORKING

--- System/Client/V14_Noblox_Modules/blacklist.js
+++ System/Client/V14_Noblox_Modules/blacklist.js
@@ -23,14 +23,18 @@ module.exports = {
 					{ name: "Add", value: "add" },
 					{ name: "Remove", value: "remove" }
 				),
+        )
+        .addStringOption((option) =>
             option
 				.setName("type")
 				.setDescription("<add/remove>")
 				.setRequired(true)
 				.addChoices(
-					{ name: "User", value: "user" },
-					{ name: "Group", value: "group" }
+					{ name: "User", value: "users" },
+					{ name: "Group", value: "groups" }
 				),
+            )
+        .addStringOption((option) =>
             option
                 .setName('input')
                 .setDescription('User ID / Group ID')
@@ -47,12 +51,12 @@ module.exports = {
         if (addorremove == "add"){
 			doAddOrRemoveMathematicReasoning(true)
 		} else if (addorremove == "remove") {
-			ref = db.ref("blacklist/users");
 			doAddOrRemoveMathematicReasoning(false)
 		}
 
         async function doAddOrRemoveMathematicReasoning(value) {
             const type = interaction.options.getString("type");
+            console.log(type)
             if (type == "groups"){
                 ref = db.ref("blacklist/groups");
                 doTypeReference(value, ref)
@@ -64,77 +68,42 @@ module.exports = {
 
         async function doTypeReference(value, ref){
             const type = interaction.options.getString("input");
-            try {
-                const typeNumberValue = Number(type)
-                doInputBlacklist(value, ref, typeNumberValue)
-            } catch (error) {
-                console.log(error)
-            }
+            const typeNumberValue = Number(type)
+            console.log(typeNumberValue)
+            doInputBlacklist(value, ref, typeNumberValue)
         }
 
         async function doInputBlacklist(value, ref, typeNumberValue) {
-
-
-        }
-
-        if (args[0] == 'add'){ 
-            addorremove('add')
-        } else if (args[0] == 'remove') {
-            addorremove('remove')
-        } else {
-            return message.reply (`You didn't provide an argument to add or remove! Try "aa>help blacklist" for this commands usage.`)
-        }
-        function addorremove(value){
-            
-            if (args[1] == 'group') {
-
-                if (!isNum(args[2])) return message.reply(`You didn't provide an argument that's a number! Try "aa>help blacklist" for this commands usage.`)
-                axios.get('https://groups.roblox.com/v1/groups/' + args[1])
-                    .then(function (response) {
-                       if (!response.data.errors) {
-                            var groupName = response.data.name
-                            var workinEmbed = new Discord.MessageEmbed()
-                        		.setDescription(`Working on the blacklist...`);
-                            message.channel.send(workinEmbed).then(message => message.delete({ timeout: 1000, reason: "delete working message" }));
-                            axios.get(`${process.env.SA_DATABASEURL}blacklist/groups/${args[1]}.json`)
-                                .then(function (response) {
-                                    console.log(response.data)
-                                    if (!response.data){
-                                        gbit(true);
-                                    }else{
-                                        gbit(false);
-                                    }
-                                })
-                
-                            function gbit(anothervalue){
-                                if (anothervalue === false && value == 'add'){return message.channel.send(groupName + ` is already a blacklisted group!`)}
-                                if (anothervalue === false && value == 'add'){return message.channel.send(groupName + ` is already a blacklisted group!`)}
-                                db.ref(`blacklist/groups/${args[1]}`).set({
-                                  name: groupName
-                                });
-                                var doneEmbed = new Discord.MessageEmbed()
-                                    .setColor(0xFF8C00)
-                                    .setDescription(`Created ${rblx_username}'s profile`)
-                                return message.channel.send(doneEmbed)
-                            }
-                       } else {
-                            return message.reply (`You didn't provide an existing group's id!`)
-                       }
+            const type = interaction.options.getString("type");
+            try {
+                console.log(typeNumberValue)
+                if (type == "groups"){
+                    db.ref(`blacklist/groups/GROUP_${typeNumberValue}`).set({
+                        id: typeNumberValue
                     });
-    
-            } else if (args[1] == 'user') {
-                
-            } else {
-                    return message.reply (`You didn't provide an argument for whether you want to do a user or group! Try "aa>help blacklist" for this commands usage.`)
+                    replyToUser(value, ref, typeNumberValue)
+                } else if (type == "users") {
+                    db.ref(`blacklist/users/GROUP_${typeNumberValue}`).set({
+                        id: typeNumberValue
+                    });
+                    replyToUser(value, ref, typeNumberValue)
+                }
+            } catch (error) {
+                console.log(error)
             }
-            axios.get('https://groups.roblox.com/v1/groups/' + args[1])
-                .then(function (response) {
-                   if (!response.data.errors) {
-                       getRobloxUsername(true, response.data.roblox_id)
-                   } else {
-                       return message.reply
-                   }
+
+        }
+        async function replyToUser(value, ref, typeNumberValue) {
+            const type = interaction.options.getString("type");
+            if (type == "groups"){
+                interaction.reply({
+                    content: `Blacklisted Group ID: ${typeNumberValue}`,
                 });
+            } else if (type == "users") {
+                interaction.reply({
+                    content: `Blacklisted User ID: ${typeNumberValue}`,
+                });
+            }
         }
     }
 }  
