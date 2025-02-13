9e2974ad630ba9bae8f8eb24a78cce8d7c6e542c
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-02-13 06:27:29 -0600

    Update roblox-join-service.js

--- Archive/roblox-join-service.js
+++ Archive/roblox-join-service.js
@@ -3,8 +3,14 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const noblox = require("noblox.js");
 
-function RJS(client) {
-	
+function RJS(client,
+	noblox,
+	currentUser,
+	admin,
+	token,
+	applicationid,
+	prefix) {
+
 }
 
 module.exports = RJS;
