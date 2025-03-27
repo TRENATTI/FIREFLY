110b326ff354b05771c528aeb233bc98890914b2
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-03-27 08:42:52 -0500

    Update ears.js -> UPDATE FORMATTING

--- System/Client/Phrases/ears.js
+++ System/Client/Phrases/ears.js
@@ -3,8 +3,8 @@ module.exports = {
 	aliases: ["im all ears","i'm all ears"],
 	wildcard: true,
 	execute(message) {
-            const imagelist = ["https://i.gyazo.com/da6f37e4d3ab4029e0d3cf5b1435d47e.png", "https://i.gyazo.com/c2817d20fef2bb89ae9cb1eb0533f358.jpg"]
+    	const imagelist = ["https://i.gyazo.com/da6f37e4d3ab4029e0d3cf5b1435d47e.png", "https://i.gyazo.com/c2817d20fef2bb89ae9cb1eb0533f358.jpg"]
 	    const imagenumber = Math.floor(Math.random() * imagelist.length)
-            return message.channel.send(imagelist[imagenumber]);
+        return message.channel.send(imagelist[imagenumber]);
 	},
 };
