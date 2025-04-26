750d1157158ec488ac46d9b9dc3faa99cdb0aa98
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-04-26 08:34:23 -0500

    Update ears.js -> MORE EARS

--- System/Client/Phrases/ears.js
+++ System/Client/Phrases/ears.js
@@ -3,7 +3,17 @@ module.exports = {
 	aliases: ["im all ears","i'm all ears"],
 	wildcard: true,
 	execute(message) {
-    	const imagelist = ["https://i.gyazo.com/da6f37e4d3ab4029e0d3cf5b1435d47e.png", "https://i.gyazo.com/c2817d20fef2bb89ae9cb1eb0533f358.jpg","https://cdn.discordapp.com/attachments/1074088257266122783/1350004596931690546/caption.gif?ex=67e64cc1&is=67e4fb41&hm=dcb17e57100734e978d9d9e74d4414411e6a699d1991d8e70fb2f5f4cf4c620d&"]
+    	const imagelist = ["https://i.gyazo.com/da6f37e4d3ab4029e0d3cf5b1435d47e.png", 
+			"https://i.gyazo.com/c2817d20fef2bb89ae9cb1eb0533f358.jpg",
+			"https://imgur.com/E5LJhoD", 
+			"https://imgur.com/9GL9m70", 
+			"https://imgur.com/ZcWde1s",
+			"https://tenor.com/view/cat-ears-big-ears-heard-dontdox-gif-8768405185234347446",
+			"https://tenor.com/view/cuddles-cuddle-cat-cat-cuddle-sleepy-cat-cat-sleepy-gif-13127427148831486106",
+			"https://tenor.com/view/cat-cats-cat-sleep-sleep-sleeping-gif-23371265",
+			"https://tenor.com/view/cat-cheese-gif-21052324",
+			
+		]
 	    const imagenumber = Math.floor(Math.random() * imagelist.length)
         return message.channel.send(imagelist[imagenumber]);
 	},
