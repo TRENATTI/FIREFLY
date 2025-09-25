778eb9aa6bc9d7cf94d4d462ad2d1c4c11ab5364
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-09-25 17:42:57 -0500

    Update seal.js -> MORE SEAL

--- System/Client/Phrases/seal.js
+++ System/Client/Phrases/seal.js
@@ -5,7 +5,17 @@ module.exports = {
 	nocooldown: true,
 	execute(message) {
 		const imagelist = [
-			"https://imgur.com/c14mP0k"
+			"https://imgur.com/c14mP0k",
+			"https://imgur.com/mldS2DA",
+			"https://imgur.com/bP8aylA",
+			"https://imgur.com/HAxoWZW",
+			"https://imgur.com/Du0tajH",
+			"https://imgur.com/zYlFBuh",
+			"https://imgur.com/ihH0YBp",
+			"https://imgur.com/QpjNZFm",
+			"https://imgur.com/WxEn7mk",
+			"https://imgur.com/a33u8ev",
+			"https://imgur.com/f9EeHaH"
 		]
 		const imagenumber = Math.floor(Math.random() * imagelist.length)
 		return message.channel.send(imagelist[imagenumber]);
