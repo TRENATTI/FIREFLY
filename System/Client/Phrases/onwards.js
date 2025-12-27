5a33ed3531ac94a2e78b20a42ca8760890e762f6
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-12-27 15:19:13 -0600

    Update onwards.js -> ADD Aliases & Change GIF/Tenor

--- System/Client/Phrases/onwards.js
+++ System/Client/Phrases/onwards.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "onwards",
-	aliases: [],
+	aliases: ["onward"],
 	execute(message) {
-		return message.channel.send("https://tenor.com/view/capybara-crocodile-alligator-riding-on-the-way-gif-27496959");
+		return message.channel.send("https://tenor.com/view/capybara-wearing-crocs-gif-1831068075687129857");
 	},
 };
