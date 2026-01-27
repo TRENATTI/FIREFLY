ee0b071371a2322930dbde837c9a5d5c15321527
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 21:25:36 -0600

    Create plonky.js -> CREATE

---
+++ src/client/discord/command-services/phrases/plonky.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "plonky",
+	aliases: [],
+	wildcard: true,
+	execute(message) {
+		return message.channel.send(
+			"https://tenor.com/view/plonk-twitch-cat-gif-27494506"
+		);
+	},
+};
