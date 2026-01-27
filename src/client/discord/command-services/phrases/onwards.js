04be29e5238dbeb1ce7dd1154c043bb3bfc338fe
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 21:25:25 -0600

    Create onwards.js -> CREATE

---
+++ src/client/discord/command-services/phrases/onwards.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "onwards",
+	aliases: ["onward"],
+	execute(message) {
+		return message.channel.send("https://tenor.com/view/capybara-wearing-crocs-gif-1831068075687129857");
+	},
+};
