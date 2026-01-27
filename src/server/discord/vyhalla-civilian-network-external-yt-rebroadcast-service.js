a4551bc800242a8fa8b86030975d5808e600d074
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 21:57:29 -0600

    Create vyhalla-internal-broadcast-service.js -> CREATE

---
+++ src/server/discord/vyhalla-civilian-network-external-yt-rebroadcast-service.js
@@ -0,0 +1,61 @@
+const { EmbedBuilder } = require("discord.js");
+const { format } = require("path");
+require("dotenv").config();
+const { stringify } = require("querystring");
+async function CCS(client, noblox, currentUser, admin) {
+	var db = admin.database();
+	client.on("messageCreate", async (message) => {
+		if (message.channel.id !== `1402763103803801721`) return;
+		const ARGS = message.content.split("&&")
+		const URL = ARGS[0]
+		const TITLE = ARGS[1]
+		if (TITLE.toLowerCase().search("vyhalla") !== -1 && TITLE.toLowerCase().search("[livestream]" !== -1)) {
+			try {
+				const guild = await client.guilds.fetch(
+					`1314823843315187742`
+				);
+				if (guild.id) {
+					const channel = await guild.channels.fetch(
+						`1402875758841696271`
+					);
+					console.log(
+						new Date(),
+						"| vyhalla-civilian-network-external-yt-rebroadcast-service.js | ",
+						channel.id,
+						`acquired.`
+					);
+					return channel.send({
+						content: `A past developer session has been archived! ${URL} <@&1402785004060414133>`
+					})
+				}
+			} catch (error) {
+				console.log(new Date(), error);
+			}
+		}
+		if (TITLE.toLowerCase().search("vyhalla") !== -1 && TITLE.toLowerCase().search("[livestream]" == -1)) {
+			try {
+				const guild = await client.guilds.fetch(
+					`1314823843315187742`
+				);
+				if (guild.id) {
+					const channel = await guild.channels.fetch(
+						`1402875758841696271`
+					);
+					console.log(
+						new Date(),
+						"| vyhalla-civilian-network-external-yt-rebroadcast-service.js | ",
+						channel.id,
+						`acquired.`
+					);
+					return channel.send({
+						content: `A new developer release has been showcased! ${URL} <@&1394311870814486649>`
+					})
+				}
+			} catch (error) {
+				console.log(new Date(), error);
+			}
+		}
+	});
+}
+
+module.exports = CCS;
