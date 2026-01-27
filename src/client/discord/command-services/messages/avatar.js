a4ba8ee69fc797d14164418be1a6dd9a7970e781
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 22:14:45 -0600

    Update avatar.js -> UPDATE PARAMS

--- src/client/discord/command-services/messages/avatar.js
+++ src/client/discord/command-services/messages/avatar.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "avatar",
 	description: "Avatar.",
 	aliases: ["av"],
-	execute(message, args) {
+	execute(message, args, client, noblox, admin) {
 		const user = message.mentions.users.first() || message.author;
 		return message.reply(
 			`\`\`${user.tag}\`\`'s avatar: ${user.displayAvatarURL({
