6136788b1ad71a9d1e7d07baca9c4ea860114405
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 22:15:03 -0600

    Update convertcelcius.js -> UPDATE CC PARAMS

--- src/client/discord/command-services/messages/convertcelcius.js
+++ src/client/discord/command-services/messages/convertcelcius.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "convertcelcius",
 	description: "Converts Celcius into Fahrenheit.",
 	aliases: ["cc"],
-	execute(message, args) {
+	execute(message, args, client, noblox, admin) {
 		if (!isNaN(args[0])) {
 			return message.reply(
 				Math.round(((9 / 5) * args[0] + 32) * 100) / 100 +
