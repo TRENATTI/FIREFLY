720404c9b52ff34e5f6bef4f6b1fe366c423387b
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-27 05:09:47 -0600

    Update meow.js -> Add wildcard property.

--- src/client/discord/command-services/phrases/meow..js
+++ src/client/discord/command-services/phrases/meow..js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "meow.",
     aliases: [],
+    wildcard: false,
     execute(message) {
         message.channel.send({ files: [
                                  "https://github.com/Alpha-Authority/Szeebe/raw/main/Assets/Meow.mp4.mp4",
