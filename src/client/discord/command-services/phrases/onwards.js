ea4549c71c6f45fbab069faf9715d622917a65ff
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-27 05:11:48 -0600

    Update onwards.js -> Add wildcard property.

--- src/client/discord/command-services/phrases/onwards.js
+++ src/client/discord/command-services/phrases/onwards.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: "onwards",
 	aliases: ["onward"],
+	wildcard: false,
 	execute(message) {
 		return message.channel.send("https://tenor.com/view/capybara-wearing-crocs-gif-1831068075687129857");
 	},
