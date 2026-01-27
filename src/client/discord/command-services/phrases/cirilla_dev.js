25206a93fdc2d422af74a65615657890ee32d10f
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-27 05:09:12 -0600

    Update cirilla_dev.js -> Add wildcard property.

--- src/client/discord/command-services/phrases/cirilla_dev.js
+++ src/client/discord/command-services/phrases/cirilla_dev.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: "cirilla_dev",
 	aliases: ["i blame cirilla_dev","i blame cirilla dev"],
+	wildcard: false,
 	execute(message) {
 		return message.channel.send(
 			"My teeth are wittawy hwitting the gwitty rn"
