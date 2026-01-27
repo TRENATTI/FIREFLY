71d527334153dc424f13b0b1debaadfb81742d76
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-01-26 21:26:43 -0600

    Create ears.js -> CREATE

---
+++ src/client/discord/command-services/phrases/ears.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: "ears",
+	aliases: ["im all ears","i'm all ears"],
+	wildcard: true,
+	nocooldown: true,
+	execute(message) {
+    	const imagelist = ["https://imgur.com/E5LJhoD", 
+			"https://imgur.com/9GL9m70",
+			"https://imgur.com/ZcWde1s", 
+			"https://imgur.com/C4Qy5rp", 
+			"https://imgur.com/nWROOfW",
+			"https://imgur.com/7HybEYy",
+			"https://imgur.com/AqZWolo",
+			"https://imgur.com/2XRjP7E",
+			"https://imgur.com/0H0DQvg",
+			"https://imgur.com/mBjp5kS",
+			"https://imgur.com/4JARohg",
+			"https://imgur.com/FldO8GG",
+			"https://imgur.com/zYGwmg9",
+			"https://imgur.com/PQ9m5HK"
+		]
+	    const imagenumber = Math.floor(Math.random() * imagelist.length)
+        return message.channel.send(imagelist[imagenumber]);
+	},
+};
