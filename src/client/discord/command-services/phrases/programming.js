1783ad026cca40f49890cfa2020b624dc01504d8
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-05-08 06:50:07 -0500

    Added programming.js

---
+++ src/client/discord/command-services/phrases/programming.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "programming",
+	aliases: ["scripting", "program"],
+	wildcard: false,
+	execute(message) {
+		return message.channel.send("https://imgur.com/a/bjTb5io");
+	},
+};
