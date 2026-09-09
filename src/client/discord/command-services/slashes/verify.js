d2d6ae1a21bf392c086a28427d39a1bdbd07a633
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-09-09 13:57:19 -0500

    Update verify.js

--- src/client/discord/command-services/slashes/verify.js
+++ src/client/discord/command-services/slashes/verify.js
@@ -1,5 +1,17 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, 
+    ContainerBuilder, 
+    LabelBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    UserSelectMenuBuilder, 
+    ButtonStyle,
+    TextInputStyle,
+    MessageFlags } = require("discord.js");
 require("dotenv").config();
+
+const {
+    verify_container
+ } = require(`./embeds/verify.js`)
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,38 +25,88 @@ module.exports = {
 	async execute(interaction, noblox, admin) {
         const db = admin.database();
 
-		try {
-            async function checkRealtimeData(nodePath) {
-                const ref = db.ref(nodePath);
-                const snapshot = await ref.once('value');
+        const snapshot = await db
+                    .ref("system")
+                    .child("user_verification")
+                    .child(`discord_${interaction.user.id}`)
+                    .once("value");
 
-                if (snapshot.exists()) { 
-                    console.log("Data exists:", snapshot.val());
-                    return true;
-                } else {
-                    console.log("No data at this path.");
-                    return false;
+        const code = makeStatecode(10)
+
+        snapshot.ref('verificationChannelID').set(`${interaction.channel.id}`)
+        snapshot.ref("verified").set(false)
+        snapshot.ref("statecode").set(`${code}`)
+        snapshot.ref("discordID").set(`${interaction.user.id}`)
+
+        async function verify_button_collector_function(responseObject) {
+            const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+            try {
+                const confirmation =
+                    await responseObject.resource.message.awaitMessageComponent({
+                        filter: collectorFilter,
+                        time: 60_000
+                    });
+
+                if (confirmation.customId !== 'verify_container_roblox_button') {
+                    return;
                 }
-            }
-
-            const snapshot = await db
-                .ref("system")
-                .child("user_verification")
-                .child(`discord_${interaction.user.id}`)
-                .once("value");
             
-            if (snapshot.exists()) {
-                verifyUser(true) // Restart Auth Data
-            } else {
-                verifyUser(false) // Start New Auth Data
+            } catch (error) {
+                console.warn(
+                    new Date,
+                    error
+                )
             }
-
-            async function verifyUser(flag) {
-                console.log(new Date(), 
-                flag)
-            }
-        } catch (error) {
-
         }
-	},
+
+        async function makeStatecode(length) {
+            var result           = '';
+            var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            
+            for ( var i = 0; i < length; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            
+            return result;
+        }
+
+        const response = await interaction.reply({
+                components: [
+                    new ContainerBuilder()
+                        .setAccentColor(0x0099ff)
+
+                        .addTextDisplayComponents(
+                            (textDisplay) =>
+                                textDisplay.setContent(
+                                    '# Verify',
+                                ),
+                        )
+                        .addSeparatorComponents((separator) => separator)
+                        .addSectionComponents((section) =>
+                                    section
+                                        .addTextDisplayComponents(
+                                            (textDisplay) =>
+                                                textDisplay.setContent(
+                                                    '## Roblox Account Verifier\nClick the button to verify your Roblox Account.',
+                                                ),
+                                        )
+                                        .setButtonAccessory((button) =>
+                                            button
+                                                .setCustomId('verify_container_roblox_button')
+                                                .setLabel('Verify')
+                                                .setStyle(ButtonStyle.Link)
+                                                .setURL(`https://authorize.roblox.com/?client_id=${process.env.ROBLOX_OAUTH2_CLIENTID}&response_type=Code&redirect_uri=https://auth.trenati.dev/redirect&scope=openid+profile&state=${code}`)
+                                                .setDisabled(false),
+                                    )
+                                ),
+                ],
+                flags: MessageFlags.IsComponentsV2,
+                withResponse: true,
+            });
+
+        await verify_button_collector_function(response)
+            
+	}
 };
