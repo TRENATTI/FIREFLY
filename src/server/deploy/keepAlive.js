48479901e4624b6a7bacdf4451416ea4b992b8d8
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2026-09-09 11:50:30 -0500

    Update keepAlive.js

--- src/server/deploy/keepAlive.js
+++ src/server/deploy/keepAlive.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const server = express();
 
-server.all('/', (req, res)=>{
-    res.send('Your bot is alive!');
+server.all('/', (req, res) => {
+	res.send('Welcome to the Roblox Verification API! This is not a website, but an API.')
 })
+
+app.get('/redirect', async ( req, res ) => {
+    res.send(`In Testing :)`)
+});
 
 function keepAlive(){
     server.listen(3000, ()=>{console.log(new Date(),
